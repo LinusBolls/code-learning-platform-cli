7f7f7bb1e13184e1f51adb704f7b238750805c34
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2024-03-09 20:52:23 +0100

    feat: make app work with bun (input doesn't get registered though)

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,16 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './app.js';
 
+// @ts-expect-error
+const runtimeIsBun = typeof Bun !== 'undefined';
+
 render(
   <QueryClientProvider client={new QueryClient()}>
     <App />
-  </QueryClientProvider>
+  </QueryClientProvider>,
+  /**
+   * Bun is a javascript runtime (meadning a modern alternative to NodeJs), that has first-class typescript support, among other advantages.
+   * however, some NodeJs apis are not yet supported by Bun. one of these is the `console.Console` constructor, which is used by `ink` to patch the console.
+   */
+  { patchConsole: !runtimeIsBun }
 );
